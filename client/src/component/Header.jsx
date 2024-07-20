2616a74d7c1ec3e07be57a0e25e47334bb1682f7
VSRahul789 <sairahul.k64@gmail.com>
2024-07-21 00:51:52 +0530

    PATCH: Fixed minor ui for better user experience

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,18 +1,45 @@
+import { useState, useEffect } from 'react';
 import { Box, Flex, Heading } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlHeader = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) {
+        // If we are scrolling down, hide the header
+        setShowHeader(false);
+      } else {
+        // If we are scrolling up, show the header
+        setShowHeader(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlHeader);
+
+      return () => {
+        window.removeEventListener('scroll', controlHeader);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
     <Flex
       pos={"fixed"}
-      top={0}
+      top={showHeader ? 0 : '-100px'}  // Adjust the height of the header as needed
       left={0}
       right={0}
       zIndex={1}
       p={4}
       justify={"space-between"}
       align={"center"}
-      bg={"black"} // Added background color
+      transition={"top 0.3s"}
     >
       <Box />
       <Link to="/" style={{ textDecoration: "none" }}>
